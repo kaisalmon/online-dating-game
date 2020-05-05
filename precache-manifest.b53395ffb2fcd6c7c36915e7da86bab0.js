09cfe9444f43a08e4eae507702612a226631b2ff
Kai Salmon <kaisalmon@hotmail.com>
2020-05-05 02:18:31 +0100

    Updates

---
+++ precache-manifest.b53395ffb2fcd6c7c36915e7da86bab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff412442eeb4d900974167f7e8810bae",
+    "url": "/online-dating-game/index.html"
+  },
+  {
+    "revision": "9b1840beb6a4a03ce928",
+    "url": "/online-dating-game/static/css/main.3c1bc35a.chunk.css"
+  },
+  {
+    "revision": "2c7e4a93c1c9ee8c825a",
+    "url": "/online-dating-game/static/js/2.2df21cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-dating-game/static/js/2.2df21cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1840beb6a4a03ce928",
+    "url": "/online-dating-game/static/js/main.18bee941.chunk.js"
+  },
+  {
+    "revision": "ab97d0407cdd9a7c5057",
+    "url": "/online-dating-game/static/js/runtime-main.c41b401c.js"
+  },
+  {
+    "revision": "6aa6c63b90bf771ec5716b730f37542d",
+    "url": "/online-dating-game/static/media/phone-wrapper.6aa6c63b.png"
+  }
+]);
